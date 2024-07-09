c3dc18b47983eeaae9e26b64b45ce2ff5deeb2f3
Dong-Han <104218943+Han961004@users.noreply.github.com>
2024-07-09 14:41:49 +0900

    Revert "강원대_Android_김동한_2주차과제_1단계 (#64)"
    This reverts commit b6d603f3cd1eade77489f857a08861057cbec6f1.

--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -2,27 +2,10 @@ package campus.tech.kakao.map
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import campus.tech.kakao.map.databinding.ActivityMainBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var sqliteInit: SQLiteInit
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        // Initialize SQLite database
-        sqliteInit = SQLiteInit(this)
-
-        // Now you can launch coroutines from here
-        lifecycleScope.launch(Dispatchers.IO) {
-            sqliteInit.insertSampleData()
-        }
+        setContentView(R.layout.activity_main)
     }
 }
